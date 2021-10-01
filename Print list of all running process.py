804bfda6681444448787aefecdabd0e992fe7f66
Pooja Marje <79684794+PoojaM-eng@users.noreply.github.com>
2021-10-01 11:01:20 +0200

    Add files via upload

---
+++ Print list of all running process.py
@@ -0,0 +1,25 @@
+import psutil
+from sys import *
+
+
+def ProcessDisplay():
+    print("list of running processes ")
+
+    Data=[]
+    for proc in psutil.process_iter():
+        value=proc.as_dict(attrs=['pid','name','username'])
+        Data.append(value)
+    return Data
+
+def main():
+    print("Script title : " + argv[0])
+
+
+    arr=ProcessDisplay()
+
+    for ele in arr:
+        print(ele)
+
+
+if __name__ == "__main__":
+    main()
